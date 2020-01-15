d97ac0a1d41672a2b04c90e7bfe107348c60185a
Marina Kogan <marina.kogan@oracle.com>
2020-01-15 02:54:47 +0000

    changed hostname based on cluster4

--- integration-tests/src/test/java/oracle/kubernetes/operator/ItMonitoringExporter.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/ItMonitoringExporter.java
@@ -1170,7 +1170,7 @@ public class ItMonitoringExporter extends BaseTest {
     podName = getPodName("app=grafana", "monitoring");
     TestUtils.checkPodReady(podName, "monitoring");
 
-    String myhost = TestUtils.getHostName();
+    String myhost = domain.getHostNameForCurl();
     LoggerHelper.getLocal().log(Level.INFO, "installing grafana dashboard");
     crdCmd =
         " cd "
@@ -1191,7 +1191,7 @@ public class ItMonitoringExporter extends BaseTest {
         + monitoringExporterEndToEndDir
         + " && "
         + "curl -v  -H 'Content-Type: application/json' "
-        + " -X GET http://admin:12345678@$HOSTNAME:31000/api/dashboards/db/weblogic-server-dashboard";
+        + " -X GET http://admin:12345678@" + myhost + ":31000/api/dashboards/db/weblogic-server-dashboard";
     ExecResult result = ExecCommand.exec(crdCmd);
     assertTrue(result.stdout().contains("wls_jvm_uptime"));
     assertTrue(
