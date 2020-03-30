bd9f7f3e4dcee03fbe2670577524262af68a0207
sankar <sankarpn@gmail.com>
2020-03-30 11:22:18 -0700

    adding test assertions

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/TestAssertions.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/assertions/TestAssertions.java
@@ -13,42 +13,101 @@ import oracle.weblogic.kubernetes.assertions.impl.Kubernetes;
 // not go direct to the impl classes
 public class TestAssertions {
 
+  /**
+   * Check if Operator is running
+   * @param namespace in which is operator is running
+   * @return true if running false otherwise
+   */
   public static Callable<Boolean> operatorIsRunning(String namespace) {
     return Operator.isRunning(namespace);
   }
 
+  /**
+   * check if operator Rest service is running
+   * @param namespace the operator rest service exists
+   * @return true if rest service is running otherwise false
+   */
   public static boolean operatorRestServiceRunning(String namespace) {
     return Operator.isRestServiceCreated(namespace);
   }
 
+  /**
+   * check if a weblogic custom resource domain exists in specified namespace and all its pods are running
+   * @param domainUID ID of the domain
+   * @param namespace in which the domain custom resource exists
+   * @return true if domain exists and pods running otherwise false
+   */
   public static Callable<Boolean> domainExists(String domainUID, String namespace) {
     return Domain.exists(domainUID, namespace);
   }
 
+  /**
+   * check if a kubernetes pod is in running/ready state
+   * @param podName name of the pod to check for
+   * @param domainUID weblogic domain uid in which the pod belongs
+   * @param namespace in which the pod is running
+   * @return true if the pod is running otherwise false
+   */
   public static Callable<Boolean> podReady(String podName, String domainUID, String namespace) {
     return Kubernetes.podRunning(podName, domainUID, namespace);
   }
 
+  /**
+   * check if a pod given by the podName is in Terminating state
+   * @param podName name of the pod to check for Terminating status
+   * @param domainUID weblogic domain uid in which the pod belongs
+   * @param namespace in which the pod is running
+   * @return true if the pod is terminating otherwise false
+   */
   public static Callable<Boolean> podTerminating(String podName, String domainUID, String namespace) {
     return Kubernetes.podTerminating(podName, domainUID, namespace);
   }
 
+  /**
+   * check is a service exists in given namespace
+   * @param serviceName  the name of the service to check for
+   * @param namespace in which the service is running
+   * @return true if the service exists otherwise false
+   */
   public static boolean serviceReady(String serviceName, String namespace) {
     return Kubernetes.serviceCreated(serviceName, namespace);
   }
 
+  /**
+   * check if a loadbalancer pod is ready
+   * @param domainUID id of the weblogic domain custom resource domain
+   * @return
+   */
   public static boolean loadbalancerReady(String domainUID) {
     return Kubernetes.loadBalancerReady(domainUID);
   }
 
+  /**
+   * check if the admin server pod is ready
+   * @param domainUID id of the domain in which admin server pod is running
+   * @param namespace in which the pod exists
+   * @return true if the admin server is ready otherwise false
+   */
   public static boolean adminServerReady(String domainUID, String namespace) {
     return Kubernetes.adminServerReady(domainUID, namespace);
   }
 
+  /**
+   * check if a adminserver T3 channel is accessible
+   * @param domainUID id of the domain in which admin server pod is running
+   * @param namespace in which the weblogic server pod exists
+   * @return
+   */
   public static boolean adminT3ChannelAccessible(String domainUID, String namespace) {
     return Domain.adminT3ChannelAccessible(domainUID, namespace);
   }
 
+  /**
+   * check if a admin server pod admin node port is accessible
+   * @param domainUID
+   * @param namespace
+   * @return
+   */
   public static boolean adminNodePortAccessible(String domainUID, String namespace) {
     return Domain.adminNodePortAccessible(domainUID, namespace);
   }
